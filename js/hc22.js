0fb0ade3c7459638e1cbb8849af8568e718190a6
andeaale <andeaale@online.no>
2015-06-17 17:12:28 +0200

    Prosjekt start

---
+++ js/hc22.js
@@ -0,0 +1,64 @@
+
+    $(function () {
+   // $('#container').highcharts({
+    var chart;
+
+
+    $(document).ready(function() {
+        $.getJSON("data/data22.php", function(json) {
+        
+            chart = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'container',
+                    type: 'line',
+                    marginRight: 130,
+                    marginBottom: 25
+                },
+                title: {
+                    text: 'Humidity vs Temperature',
+                    x: -20 //center
+                },
+                subtitle: {
+                    text: '',
+                    x: -20
+                },
+                xAxis: {
+                    categories: []//['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                yAxis: {
+                    title: {
+                        text: ' '//'Amount'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                tooltip: {
+                    formatter: function() {
+                            return '<b>'+ this.series.name +'</b><br/>'+
+                            this.x +': '+ this.y;
+                    }
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'top',
+                    x: -10,
+                    y: 100,
+                    borderWidth: 0
+                },
+                series: json
+            });
+        });
+    });
+    });
+    
+//});
+
+
+
+
+
+
